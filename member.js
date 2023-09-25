73ece3000316eacb03206ce15fb934261af620e9
Bruno Gomes <brunoshy@gmail.com>
2023-09-25 04:27:32 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,12 @@
 function skillsMember() {
-    var skills = document.getElementById("skills").value;
-    var skillsError = document.getElementById("skillsError");
-    var skillsRegex = /^[a-zA-Z0-9_]{3,20}$/;
-    if (skillsRegex.test(skills) == true) {
-        skillsError.innerHTML = "";
-        return true;
-    } else {
-        skillsError.innerHTML = "Invalid skills";
-        return false;
-    }
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
 }
